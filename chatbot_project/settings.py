5e041c50257b2c63201aba5cb2b59fc4dfe2bd2e
Maryann Xu <mxxu@usc.edu>
2023-09-10 01:30:51 -0700

    fixed

--- chatbot_project/settings.py
+++ chatbot_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ukg+_y*wmsa(_05n=zp=uviwwo^56lxnk&s9z%4+$(va*_wdk9'
+SECRET_KEY = ''
 
 OPENAI_API_KEY = ''
 
